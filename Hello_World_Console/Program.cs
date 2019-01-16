ada78fc0ea26c68f34dc1f6320c3663d5ad074b1
Vivekkumar260892 <vivekkumar1@mail.usf.edu>
2019-01-15 20:54:28 -0500

    New V2

--- Hello_World_Console/Program.cs
+++ Hello_World_Console/Program.cs
@@ -6,7 +6,50 @@ namespace Hello_World_Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.Write("What is your age : ");
+            try
+            {
+                string TestInput = Console.ReadLine();
+                int age = int.Parse(TestInput);
+                if  (age <= 18)
+                {
+                    Console.WriteLine("You are a minor");
+                }
+                else if (age >18 && age<=65)
+               {
+                    Console.WriteLine("You are an adult");
+                }
+                else {
+                    Console.WriteLine("You are a senior");
+                }
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey(true);
+
+                if (age <= 18)
+                {
+                    Console.WriteLine("Which is your favourite sport");
+                    string sport=Console.ReadLine();
+                    Console.WriteLine("Your favourite sport is " + sport);
+                }
+                else if (age > 18 && age <= 65)
+                {
+                    Console.WriteLine("Which is your favourite vacation destination");
+                    string sport= Console.ReadLine();
+                    Console.WriteLine("Your favourite sport is " + sport);
+                }
+                else
+                {
+                    Console.WriteLine("Which is your favourite TV show");
+                    string sport = Console.ReadLine();
+                    Console.WriteLine("Your favourite sport is " + sport);
+                }
+               
+                Console.ReadKey(true);
+            }//end of try
+            catch
+            {
+                Console.WriteLine("Invalid Age");
+            }//end of catch
+        }//end of main
     }
 }
